6eba4ba9354b6fd15b95ae7135c7235273742285
Dhruval Chauhan <dhruval.chauhan@saeculumsolutions.com>
2023-07-18 14:10:28 +0530

    array

--- july18/july18.js
+++ july18/july18.js
@@ -99,4 +99,166 @@
 // })       
 // console.log(C)
 
+//13
+
+// const employees = [
+//     { name: "John", salary: 50000, department: "IT" },
+//     { name: "Jane", salary: 60000, department: "HR" },
+//     { name: "Bob", salary: 55000, department: "IT" },
+//     { name: "Sophie", salary: 75000, department: "HR" },
+//     { name: "Mike", salary: 65000, department: "IT" },
+//     { name: "Emily", salary: 80000, department: "HR" },
+//     { name: "David", salary: 70000, department: "IT" },
+//   ];
+  
+//   Que1: => count the average salary of each department
+//   Que 2: =>  output: {
+//               IT: [50000,55000,65000,70000],
+//               HR: [60000,75000, 80000]
+//            }
+// const I=[]
+// const H=[]
+// function department (value){
+//     if(employees.department == value.department=="IT"){
+//        return value.salary
+//     }
+//     else {
+//         value.department=="HR" 
+//         // return H.push(value.salary)
+//         return value.salary
+//     }
+// }
+  
+// const depart=department(employees)
+// console.log(depart)
+// console.log(I)
+
+// console.log(department(employees))
+
+
+
+//19
+// What is the output of the following code?
+// 	let arr1 = [{a: 34}, {a: 45}, {a:12}, {a:5}, {a:78}];
+// 	let arr2 = arr1.map(item => item);
+// 	arr2[2].b = 45;
+//     arr2[2].c=300
+// console.log(arr1);  // => 
+// 	console.log(arr2);  // =>
+
+//20
+
+// What is the output of the following code?
+// let arr1 = [{a: 34}, {a: 45}, {a:12}, {a:5}, {a:78}];
+// 	let arr2 = arr1.map(item => item.a * 2);
+// console.log(arr1);  // => 
+// 	console.log(arr2);  // =>
+
+//21
+// What is the output of the following code?
+// let arr = [1,2,3,4,5];
+// let arr2 = arr.reduce((a, c) => a + c).map(item => item);
+// console.log(arr2); // =>
+
+//22
+// What is the output of the following code?
+// const example = ({ a, b, c }) => {
+// console.log(a, b, c);  
+// };
+// example(0, 1, 2);
+
+// console.log(example)
+
+//23
+// // Find the Sum of Squares 
+// let arr = [1,2,3,4,5] //=> ans : 55
+
+// const sumOfSquares =arr.reduce((accu,val)=>{
+//      accu+val 
+//      return (accu+val)+accu
+// })
+// console.log(sumOfSquares)
+
+//24
+const students = [
+    { name: 'Alice', age: 20, grade: 'A' },
+    { name: 'Bob', age: 18, grade: 'B' },
+    { name: 'Charlie', age: 19, grade: 'A' },
+    { name: 'David', age: 20, grade: 'C' },
+    { name: 'Aavid', age: 20, grade: 'C' },
+  ];
+    //   => sort by age
+    //   => sort by grade
+    //   => sort by name
+    //   => average of ages
+    //   => sum of ages
+
+
+    // const a=students.map((val)=> val.age)
+    // console.log(a.sort())
+
+    // const b=students.map((val)=>val.grade)
+    // console.log(b.sort())
+
+    // const c=students.map((val)=>val.name)
+    // console.log(c.sort())
+
+    // const d=students.reduce((acc,val)=>acc+val.age)
+    // console.log(d)
+
+    // const e=students.map((val)=>val.age)
+    // console.log(e)
+    // const f =e.reduce((ac,val)=>ac+val)
+    // console.log(f)
+
+    //26
+    // Create an array of squares of even numbers from a given array.
+    // const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    // const b=numbers.filter((val)=>val%2==0).map((val)=>val*2)
+    // console.log(b)
+
+    //27
+    // Filter out duplicate values from an array.
+    // const numbers = [1, 2, 2, 3, 4, 4, 5,0,12,17,11,10,11,10,0];
+    // const a=numbers.filter((val,ind)=>numbers.indexOf(val)===ind)
+    // console.log(a)
+
+
+    //28
+    //Find the index of the first odd number in an array.
+    // const numbers = [2, 4,1, 6, 8, 9, 10,3,5];
+    // const a=numbers.filter((val,ind)=>  val%2!==0)
+    // console.log(a)
+
+    //31
+    // Find the index of the first person whose name starts with 'A' and age is less than 20.
+//     const people = [
+//   { name: 'Alice', age: 18 },
+//   { name: 'Bob', age: 20 },
+//   { name: 'Charlie', age: 19 },
+//   { name: 'David', age: 21 },
+//   { name: 'Alif', age: 18 },
+// ];
+
+// const b=people.filter((val)=>{
+//     if(val.name[0].charAt(0)=="A" && val.age<20){
+//     console.log(val)
+//     }
+// })
+
+
+//32
+
+// make a function that takes two parameter (arrayOfArray, array) and returns boolean based on following example:
+
+fun_name([[1, 2], [34, 67], [67, 77], [88, 90]], [1, 2]) //=> true
+fun_name([[1, 2], [34, 67], [67, 77], [88, 90]], [2, 1]) //=> true
+fun_name([[1, 2], [34, 67], [67, 77], [88, 90]], [22, 15]) //=> false
+fun_name([[1, 2], [34, 67], [67, 77], [22, 90, 15], [88, 90]], [22, 15, 90]) //=> true
+
+
+  
+
+
 
